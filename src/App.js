d1e0bb05c862eaf1988f8fc243a4fbeca86cbc3f
Mazen El Ali <mazenelali>
2023-12-06 18:28:25 +0200

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeroSection from "./components/Hearosection";
+function App() {
+    return (
+        <div className="App">
+            <div className=" overflow-hidden">
+                <HeroSection />
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Hearosection.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "../components/Hearosection.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Slide } from "react-awesome-reveal";
+
+function HeroSection() {
+    const [onLoad, setOnLoad] = useState(false);
+
+    useEffect(() => {
+        AOS.init({
+            offset: 200,
+            duration: 600,
+            easing: "ease-in-sine",
+            delay: 100,
+        });
+        const handleLoad = () => {
+            setOnLoad(true);
+        };
+        window.addEventListener("load", handleLoad);
+        return () => {
+            window.removeEventListener("load", handleLoad);
+        };
+    }, []);
+
+    return (
+        <div className="relative w-[150%] h-screen " id="hero">
+            <div
+                className={`absolute ${
+                    onLoad
+                        ? "lg:w-[2000px] lg:h-[2000px] w-[1000px]  h-[1000px] duration-1000 ease-in-out"
+                        : "w-[100px] h-[100px]  duration-1000 ease-in-out"
+                } container -bottom-52 lg:-bottom-96 -left-52`}
+                id="bulletone"
+            ></div>
+            <div
+                className={`absolute ${
+                    onLoad
+                        ? "lg:w-[2000px] lg:h-[2000px] w-[800px] h-[800px]  duration-1000 ease-in-out"
+                        : "w-[100px] h-[100px]  duration-1000 ease-in-out"
+                } lg:-bottom-96 lg:-right-32 -top-96 -right-28 containerOne`}
+                id="bulletone"
+            ></div>
+            <div
+                data-aos="fade-down"
+                className="absolute lg:w-[42%] w-full pb-10 h-screen flex lg:items-center lg:pb-32 items-end lg:ml-24 "
+            >
+                <Slide cascade>
+                    <h2 className=" lg:p-10 p-5 lg:top-20 lg:left-20 bottom-20 lg:w-full w-[370px] left-20 text-white lg:text-5xl text-2xl lg:tracking-wide">
+                        Hi, I'm <span className="text-second">Mazen </span>, a
+                        web developer passionate about creating sleek websites{" "}
+                        <span className="text-second">together </span>!
+                    </h2>
+                </Slide>
+            </div>
+        </div>
+    );
+}
+export default HeroSection;
