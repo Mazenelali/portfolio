768b3e5c025e97f1a872175326e8436f8696de12
Mazen El Ali <mazenelali>
2023-12-13 16:52:46 +0200

    image hreo

--- src/components/Hearosection.js
+++ src/components/Hearosection.js
@@ -3,7 +3,7 @@ import "../components/Hearosection.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Slide } from "react-awesome-reveal";
-
+import hi from "../assets/hi.png";
 function HeroSection() {
     const [onLoad, setOnLoad] = useState(false);
 
@@ -25,33 +25,41 @@ function HeroSection() {
 
     return (
         <div className="relative w-[150%] h-screen " id="hero">
-            <div
-                className={`absolute ${
-                    onLoad
-                        ? "lg:w-[2000px] lg:h-[2000px] w-[1000px]  h-[1000px] duration-1000 ease-in-out"
-                        : "w-[100px] h-[100px]  duration-1000 ease-in-out"
-                } container -bottom-52 lg:-bottom-96 -left-52`}
-                id="bulletone"
-            ></div>
-            <div
-                className={`absolute ${
-                    onLoad
-                        ? "lg:w-[2000px] lg:h-[2000px] w-[800px] h-[800px]  duration-1000 ease-in-out"
-                        : "w-[100px] h-[100px]  duration-1000 ease-in-out"
-                } lg:-bottom-96 lg:-right-32 -top-96 -right-28 containerOne`}
-                id="bulletone"
-            ></div>
-            <div
-                data-aos="fade-down"
-                className="absolute lg:w-[42%] w-full pb-10 h-screen flex lg:items-center lg:pb-32 items-end lg:ml-24 "
-            >
-                <Slide cascade>
-                    <h2 className=" lg:p-10 p-5 lg:top-20 lg:left-20 bottom-20 lg:w-full w-[370px] left-20 text-white lg:text-5xl text-2xl lg:tracking-wide">
-                        Hi, I'm <span className="text-second">Mazen </span>, a
-                        web developer passionate about creating sleek websites{" "}
-                        <span className="text-second">together </span>!
-                    </h2>
-                </Slide>
+            <div>
+                <div
+                    className={`absolute ${
+                        onLoad
+                            ? "lg:w-[2000px] lg:h-[2000px] w-[1000px]  h-[1000px] duration-1000 ease-in-out"
+                            : "w-[100px] h-[100px]  duration-1000 ease-in-out"
+                    } container -bottom-52 lg:-bottom-96 -left-52`}
+                    id="bulletone"
+                ></div>
+                <div
+                    className={`absolute ${
+                        onLoad
+                            ? "lg:w-[2000px] lg:h-[2000px] w-[800px] h-[800px]  duration-1000 ease-in-out"
+                            : "w-[100px] h-[100px]  duration-1000 ease-in-out"
+                    } lg:-bottom-96 lg:-right-32 -top-96 -right-28 containerOne`}
+                    id="bulletone"
+                ></div>
+                <div
+                    data-aos="fade-down"
+                    className="absolute lg:w-[38%] w-full pb-10 pt-10 h-screen flex lg:items-center lg:pb-32 items-start lg:ml-18 "
+                >
+                    <Slide cascade>
+                        <h2 className=" lg:p-10 p-5 lg:top-20 lg:left-20 bottom-20 lg:w-full w-[370px] left-20 text-white font-bold lg:text-5xl text-2xl lg:tracking-wide">
+                            Hi, I'm <span className="lg:text-second  text-black">Mazen </span>,
+                            a web developer passionate about creating sleek
+                            websites{" "}
+                            <span className="lg:text-second text-black">together </span>!
+                        </h2>
+                    </Slide>
+                </div>
+            </div>
+            <div className=" relative w-full h-full flex  lg:items-end lg:justify-center  items-end ">
+                <div className=" right-0 lg:w-[700px] lg:h-[700px]  w-96 h-96  ">
+                    <img src={hi} alt="Mazen" className="w-full h-full" />
+                </div>
             </div>
         </div>
     );
